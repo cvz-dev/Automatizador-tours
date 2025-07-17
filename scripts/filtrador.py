577a0fa10bde785cb17cf475f8b4fef2d499a08c
cvz-dev <sebastian.rod.arellano@gmail.com>
2025-07-16 19:52:08 -0600

    Correccion del gitignore para la subida correcta de las carpetas

---
+++ scripts/filtrador.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import re
+from datetime import datetime
+
+def limpiar_fecha(fecha):
+    meses = {
+        'enero':"01", 'febrero':"02", 'marzo':"03", 'abril':"04", 
+        'mayo':"05", 'junio':"06",'julio':"07", 'agosto':"08", 
+        'septiembre':"09", 'octubre': "10", 'noviembre': "11", 'diciembre':"12" 
+    }
+
+    if isinstance(fecha, str):
+        try:
+            fecha = fecha.lower()
+            fecha = re.sub(r"(miércoles|miercoles|sabado|sábado)", "", fecha)
+            fecha = fecha.strip()
+
+            for key in meses:
+                if re.search(key,fecha):
+                    fecha = re.sub(key, meses[key], fecha)
+                    break
+
+            if not re.search(r"\b\d{4}\b", fecha):
+                año_actual = datetime.now().year
+                fecha = re.sub(r"(-)", f"{año_actual} -", fecha, count=1)
+            else:
+                fecha = re.sub(r"(\d{4})(-)", r"\1 \2", fecha)
+
+            fecha = re.sub(r"(\d{2})(\d{4})",r"\1 \2",fecha)
+            fecha = re.sub(r"(\d{1,2}:\d{2})(am|pm)", r"\1 \2", fecha)
+            fecha = datetime.strptime(fecha, "%d de %m %Y - %I:%M %p")
+
+            return fecha
+        
+        except Exception as desc:
+            return fecha
+        
+    return fecha
+
+#Definir una función para regresar dos DataFrames filtrados por cada campus
+def filtrar_datos(ruta):
+
+    #Lectura del archivo
+    df = pd.read_csv(ruta)
+
+    # Convertimos las columnas 'Día de visita Norte' y 'Día de visita Sur'
+    # al formato datetime para poder filtrar los datos más adelante
+    df['Día de visita Norte_standar'] = df['Día de visita Norte'].copy()
+    df['Día de visita Sur_standar'] = df['Día de visita Sur'].copy()
+    df['Día de visita Norte_standar'] = df['Día de visita Norte_standar'].apply(limpiar_fecha)
+    df['Día de visita Sur_standar'] = df['Día de visita Sur_standar'].apply(limpiar_fecha)
+
+    # Quitar las columnas relacionadas con el 'Día de visita sur'
+    df_filtrado_norte = df.drop(columns=['Día de visita Sur', 'Día de visita Sur_standar'])
+
+    # Quitar las columnas relacionadas con el 'Día de visita norte'
+    df_filtrado_sur = df.drop(columns=['Día de visita Norte', 'Día de visita Norte_standar'])
+     
+    # Regresar dos dataframes uno por cada campus
+    return df_filtrado_norte, df_filtrado_sur
---
+++ scripts/guardar_registros_excel.py
@@ -0,0 +1,116 @@
+import pandas as pd
+import os
+import re
+import openpyxl
+import sys
+from copy import copy
+from datetime import datetime
+from filtrador import filtrar_datos
+
+def buscar_archivo(ruta, nombre_archivo):
+    try:
+        for archivo in os.listdir(ruta):
+            if re.search(nombre_archivo,archivo):
+                return 'Encontrado'
+        else:
+            return 'No encontrado'
+    except FileNotFoundError:
+        return 'Path invalido'
+
+def existe_hoja(ruta, nombre_hoja):
+    try:
+        wb = openpyxl.load_workbook(ruta)
+        hoja_wb = wb[nombre_hoja]
+        return True
+    except KeyError:
+        return False
+
+def copiar_formato(ruta, nombre_hoja):
+    wb_nuevo = openpyxl.load_workbook(ruta)
+    hoja_nueva = wb_nuevo[nombre_hoja]
+
+    wb_plantilla = openpyxl.load_workbook('../plantilla/plantilla_tours.xlsx')
+    hoja_plantilla = wb_plantilla['Plantilla']
+
+    for columna in hoja_plantilla.column_dimensions:
+        hoja_nueva.column_dimensions[columna].width = hoja_plantilla.column_dimensions[columna].width
+        
+        for fila in hoja_plantilla.iter_rows():
+            for celda in fila:
+                nueva_celda = hoja_nueva.cell(row=celda.row, column=celda.column, value=celda.value)
+                if celda.has_style:
+                    nueva_celda.font = copy(celda.font)
+                    nueva_celda.border = copy(celda.border)
+                    nueva_celda.fill = copy(celda.fill)
+                    nueva_celda.number_format = copy(celda.number_format)
+                    nueva_celda.protection = copy(celda.protection)
+                    nueva_celda.alignment = copy(celda.alignment)
+
+    wb_nuevo.save(ruta)
+
+def copiar_datos(df, ruta, nombre_hoja, fila_inicial):
+    wb = openpyxl.load_workbook(ruta)
+    hoja_wb = wb[nombre_hoja]
+
+    for i, fila in enumerate(df.itertuples(index=False), start=fila_inicial):
+        hoja_wb.insert_rows(idx=i, amount=1)
+        for j, valor in enumerate(fila, start=1):
+            hoja_wb.cell(row=i, column=j, value=valor)
+    
+    wb.save(ruta)
+
+def guardar_registros(df, fecha_solicitada):
+    meses = {
+    "01": 'enero', "02": 'febrero', "03": 'marzo', "04": 'abril',
+    "05": 'mayo', "06": 'junio', "07": 'julio', "08": 'agosto',
+    "09": 'septiembre', "10": 'octubre', "11": 'noviembre', "12": 'diciembre'
+    }
+    
+    dia = fecha_solicitada.strftime("%d")
+    mes = meses[fecha_solicitada.strftime("%m")]
+    año = fecha_solicitada.strftime("%Y")
+    nombre_hoja = dia + " de " + mes
+    nombre_excel = mes + " " + año
+
+    busqueda = buscar_archivo("../data", nombre_excel)
+    if busqueda == 'Encontrado':
+        print('Encontrado')
+        if existe_hoja('../data/Tour puertas abiertas ' + nombre_excel + '.xlsx', nombre_hoja):
+            print('Existe la hoja')
+            copiar_datos(df, '../data/Tour puertas abiertas ' + nombre_excel + '.xlsx', nombre_hoja, 4)
+            return
+        else:
+            print('No existe la hoja')
+            wb = openpyxl.load_workbook('../data/Tour puertas abiertas ' + nombre_excel + '.xlsx')
+            wb.create_sheet(nombre_hoja)
+            wb.save('../data/Tour puertas abiertas ' + nombre_excel + '.xlsx')
+    elif busqueda == 'No encontrado' :
+        print('No encontrado')
+        wb = openpyxl.Workbook()
+        hoja_nueva = wb.active
+        hoja_nueva.title = nombre_hoja
+        wb.save('../data/Tour puertas abiertas ' + nombre_excel + '.xlsx')
+    elif busqueda == 'Path invalido':
+        print('Path invalido')
+        sys.exit(1)
+    
+    copiar_formato('../data/Tour puertas abiertas ' + nombre_excel + '.xlsx', nombre_hoja)
+    copiar_datos(df, '../data/Tour puertas abiertas ' + nombre_excel + '.xlsx', nombre_hoja, 8)
+
+existen_registros_norte = False
+existen_registros_sur = False
+
+df_norte, df_sur = filtrar_datos("../data/prueba_lectura_registros.csv")
+fecha_solicitada = datetime(2025, 5, 28).date()
+
+df_sur_fecha = df_sur[df_sur['Día de visita Sur_standar'].dt.date == fecha_solicitada]
+if not df_sur_fecha.empty:
+    df_sur_fecha = df_sur_fecha.sort_values(by='Nombre')
+    guardar_registros(df_sur_fecha, fecha_solicitada)
+    existen_registros_sur = True
+
+df_norte_fecha = df_norte[df_norte['Día de visita Norte_standar'].dt.date == fecha_solicitada]
+if not df_norte_fecha.empty:
+    df_norte_fecha = df_norte_fecha.sort_values(by='Nombre')
+    guardar_registros(df_norte_fecha, fecha_solicitada)
+    existen_registros_norte = True
